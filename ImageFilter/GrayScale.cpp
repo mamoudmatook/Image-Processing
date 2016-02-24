318659d4779f8f56d1ad37e8b4ce3833362a07a6
David Chavez <d-chavez@outlook.com>
2016-02-24 03:25:18 -0600

    WIP: Layout GrayScale

---
+++ ImageFilter/GrayScale.cpp
@@ -0,0 +1,27 @@
+#include "GrayScale.h"
+
+
+
+GrayScale::GrayScale()
+{
+}
+
+
+GrayScale::~GrayScale()
+{
+}
+
+void GrayScale::Luminosity()
+{
+	//(max(RGB) + min(RGB)) / 2
+}
+
+void GrayScale::Mean()
+{
+	//(R+G+B) / 3
+}
+
+void GrayScale::Luminance()
+{
+	//0.3R + 0.59G + 0.11B
+}
---
+++ ImageFilter/GrayScale.h
@@ -0,0 +1,12 @@
+#pragma once
+class GrayScale
+{
+public:
+	GrayScale();
+	~GrayScale();
+
+	void Luminosity();
+	void Mean();
+	void Luminance();
+};
+
