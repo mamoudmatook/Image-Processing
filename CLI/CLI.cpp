2d15764e1ce85ccc37d21a4ebe1a5328e1a153df
David Chavez <d-chavez@outlook.com>
2016-02-26 00:44:20 -0600

    Agregadas referencias a libreria para pruebas

--- CLI/CLI.cpp
+++ CLI/CLI.cpp
@@ -2,10 +2,28 @@
 //
 
 #include "stdafx.h"
-
+#include "GrayScale.h"
 
 int main()
 {
+	Cv::GrayScale* g = new Cv::GrayScale();
+	if (g->SetImage(L"C:\\Users\\David\\Documents\\Visual Studio 2015\\Projects\\PIAD1\\Debug\\lenna.jpg")) {
+		g->Luminance();
+		g->Save(L"luminance.png");
+	}
+
+	Cv::GrayScale* g1 = new Cv::GrayScale();
+	if (g1->SetImage(L"C:\\Users\\David\\Documents\\Visual Studio 2015\\Projects\\PIAD1\\Debug\\lenna.jpg")) {
+		g1->Luminosity();
+		g1->Save(L"luminosity.png");
+	}
+
+	Cv::GrayScale* g2 = new Cv::GrayScale();
+	if (g2->SetImage(L"C:\\Users\\David\\Documents\\Visual Studio 2015\\Projects\\PIAD1\\Debug\\lenna.jpg")) {
+		g2->Mean();
+		g2->Save(L"mean.png");
+	}
+
     return 0;
 }
 
