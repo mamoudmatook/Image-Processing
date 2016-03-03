9922cacc7c05ddf2c789e7e0eaf3af324d4548c3
David Chavez <david@estrateca.com>
2016-03-03 16:38:33 -0600

    Clase base

---
+++ ImageFilter/ImageProcessing.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "stdafx.h"
+
+namespace Cv {
+	
+	class ImageProcessing
+	{
+	public:
+		ImageProcessing();
+		~ImageProcessing();
+
+		bool SetImage(std::wstring fileUri);
+		bool Save(std::wstring filename, std::wstring mimeType = L"image/png");
+		Gdiplus::Bitmap* GetProcessedImage();
+
+	private:
+		Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+		ULONG_PTR gdiplusToken;
+
+		long totalPixels;
+		int imageWidth;
+		int imageHeight;
+		Gdiplus::Rect* rect;
+
+		Gdiplus::Bitmap* originalImage;
+		Gdiplus::Bitmap* processedImage;
+
+		void GetImageDimensions();
+
+		int GetEncoderClsid(const WCHAR* format, CLSID* pClsid);
+	};
+}
